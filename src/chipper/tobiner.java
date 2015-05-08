147d6e281d9888e758d08f9e7f4ba8ecb0d52958
depepe <depepe@depepe-PC>
2015-05-08 14:24:25 +0700

    tyttree

---
+++ src/chipper/tobiner.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chipper;
+
+/**
+ *
+ * @author depepe
+ */
+public class tobiner {
+    
+int abx,abfx,abmod;
+    String ab1bin,ab2bin,ab3bin,ab4bin,ab5bin,ab6bin,ab7bin,ab8bin;
+public void Hasil1(){
+    if(abx<=26){
+    abfx=(abx*4)*2;
+    abmod=abfx%37;
+    ab1bin=Integer.toBinaryString(abmod);
+    }
+    else{
+        abfx=abx;
+        abmod=abfx;
+       ab1bin=Integer.toBinaryString(abmod); 
+    }
+}
+public void Hasil2(){
+    if(abx<=26){
+    abfx=(abx*4)*2;
+    abmod=abfx%37;
+    ab2bin=Integer.toBinaryString(abmod);
+    }
+    else{
+        abfx=abx;
+        abmod=abfx;
+       ab2bin=Integer.toBinaryString(abmod); 
+    }
+}
+public void Hasil3(){
+    if(abx<=26){
+    abfx=(abx*4)*2;
+    abmod=abfx%37;
+    ab3bin=Integer.toBinaryString(abmod);
+    }
+    else{
+        abfx=abx;
+        abmod=abfx;
+       ab3bin=Integer.toBinaryString(abmod); 
+    }
+}
+public void Hasil4(){
+    if(abx<=26){
+    abfx=(abx*4)*2;
+    abmod=abfx%37;
+    ab4bin=Integer.toBinaryString(abmod);
+    }
+    else{
+        abfx=abx;
+        abmod=abfx;
+       ab4bin=Integer.toBinaryString(abmod); 
+    }
+}
+public void Hasil5(){
+    if(abx<=26){
+    abfx=(abx*4)*2;
+    abmod=abfx%37;
+    ab5bin=Integer.toBinaryString(abmod);
+    }
+    else{
+        abfx=abx;
+        abmod=abfx;
+       ab5bin=Integer.toBinaryString(abmod); 
+    }
+}
+public void Hasil6(){
+    if(abx<=26){
+    abfx=(abx*4)*2;
+    abmod=abfx%37;
+    ab6bin=Integer.toBinaryString(abmod);
+    }
+    else{
+        abfx=abx;
+        abmod=abfx;
+       ab6bin=Integer.toBinaryString(abmod); 
+    }
+}
+public void Hasil7(){
+    if(abx<=26){
+    abfx=(abx*4)*2;
+    abmod=abfx%37;
+    ab7bin=Integer.toBinaryString(abmod);
+    }
+    else{
+        abfx=abx;
+        abmod=abfx;
+       ab7bin=Integer.toBinaryString(abmod); 
+    }
+}
+public void Hasil8(){
+    if(abx<=26){
+    abfx=(abx*4)*2;
+    abmod=abfx%37;
+    ab8bin=Integer.toBinaryString(abmod);
+    }
+    else{
+        abfx=abx;
+        abmod=abfx;
+       ab8bin=Integer.toBinaryString(abmod); 
+    }
+}
+}
+
